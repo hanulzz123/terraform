ff0d0c90d1a1a94b3f395e3516d67e02589f9e68
hanulzz123 <hanulzz123@gmail.com>
2021-10-25 12:20:19 +0900

    1025 ver-2 userdata

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-setenforce 0
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install -y yum-utils
+# Updated to use Amazon Linux 2
+yum -y update
 yum-config-manager --enable remi-php74
 yum-config-manager --disable remi-php54
-yum install -y php php-cli php-mysql httpd wget
+yum install -y yum-utils httpd php mysql php-mysql wget
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 wget https://ko.wordpress.org/latest-ko_KR.tar.gz
 tar xvfz latest-ko_KR.tar.gz 
 cp -a wordpress/* /var/www/html/
 chown -R apache.apache /var/www/html/*
 sed -i 's/DirectoryIndex index.html/DirectoryIndex index.php/g' /etc/httpd/conf/httpd.conf
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sed -i 's/database_name_here/wordpress/g' /var/www/html/wp-config.php
-sed -i 's/username_here/wordpress/g' /var/www/html/wp-config.php
-sed -i 's/password_here/It12345!/g' /var/www/html/wp-config.php
-sed -i 's/localhost/10.112.208.3/g' /var/www/html/wp-config.php
+sed -i 's/database_name_here/Enter Your DB Name/g' /var/www/html/wp-config.php
+sed -i 's/username_here/Enter Your User Name/g' /var/www/html/wp-config.php
+sed -i 's/password_here/Enter Your Password/g' /var/www/html/wp-config.php
+sed -i 's/localhost/Enter Your DB Endpoint or IP/g' /var/www/html/wp-config.php
 systemctl start httpd
 systemctl enable httpd
